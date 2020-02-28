b2ee2a48b4db4b3fb8edc2767fa2a1be8ba0afb5
陈清松 <821570533@qq.com>
2020-02-28 23:21:38 +0800

    中间导航栏完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,10 +2,12 @@
 import request from '../../utils/request.js'
 Page({
   data: {
-    swiperList:[]
+    swiperList:[],
+    catitems:[]
   },
   
   onLoad(){
+    // 获取swiper轮播图的请求
       request({
         url: '/home/swiperdata'
       }).then(res=>{
@@ -13,6 +15,23 @@ Page({
           swiperList : res.data.message
         })
       })
+      // 获取导航栏的请求
+      request({
+        url:"/home/catitems"
+      }).then(res=>{
+        // 返回的数据由于跳转的路径不一样，所以需要改造数据
+        let newData = res.data.message.map((v,i)=>{
+          // 做一个小的判断，取到第一个数据
+          if(v.name === '分类'){
+            v.url = "/pages/category/index"
+          }
+          return v
+        })
+        this.setData({
+          catitems: newData
+        })
+      })
+      
   }
 
 })
