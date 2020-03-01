24a2d4cdc8fbfa8806e177fde928bbbd73883326
陈清松 <821570533@qq.com>
2020-03-01 21:59:34 +0800

    分类页面完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,30 @@
 // pages/category/index.js
+
+import request from "../../utils/request.js"
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // tab栏当前索引
+    current: 0,
+    // 分类数据
+    category:[]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 点击样式
+  clickcolor(e){
+    this.setData({
+      current: e.currentTarget.dataset.item
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad(){
+    request({
+      url:"/categories"
+    }).then(res=>{
+      console.log(res)
+      this.setData({
+        category: res.data.message
+      })
+    })
   }
 })
