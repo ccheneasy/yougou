7e853ccb04b7736328a2653f97ad46f175cbbf7f
陈清松 <821570533@qq.com>
2020-03-02 18:01:39 +0800

    bug修复

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,21 @@
-// pages/search/index.js
+import request from '../../utils/request.js'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 商品列表
+    goodsList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 输入完成事件
+  inputEvent(e){
+    
+    request({
+      url:"/goods/qsearch",
+      params:{
+        query: e.detail.value
+      }
+    }).then(res=>{
+      console.log(res)
+    })
   }
+
 })
