26806fb917528040a029d7bd36969407fedd1a70
陈清松 <821570533@qq.com>
2020-03-03 00:14:06 +0800

    页面渲染完成

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -7,7 +7,13 @@ Page({
     // 商品列表
     goodsList: [],
     // 商品总个数
-    total:''
+    total:'',
+    // 是否有更多
+    hasMore: true,
+    // 页面
+    pagenum: 1,
+    // 是否正在加载中
+    loading: true
   },
 
   onLoad(options){
@@ -15,13 +21,20 @@ Page({
     request({
       url: "/goods/search",
       data: {
-        query: name
+        query: name,
+        pagenum: this.data.pagenum, // 页数
+        pagesize: 10
       }
     }).then(res=>{
-      console.log(res)
+      // 数据改造,将价格后面加上两位小数
+      let goods = res.data.message.goods.map(v => {
+        v.goods_price = v.goods_price.toFixed(2)
+        return v
+      })
+      console.log(goods)
       this.setData({
         name:name,
-        goodsList:res.data.message.goods,
+        goodsList:goods,
         total: res.data.message.total
       })
     })
