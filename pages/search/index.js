cc50484f04044f19aa3ff37bcb4e290d3bdb7889
陈清松 <821570533@qq.com>
2020-03-03 22:18:40 +0800

    远程搜索列表的展示

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,9 +1,29 @@
-// pages/search/index.js
+import request from '../../utils/request.js'
 Page({
 
   data: {
-
+    value: '',
+    searchlist: []
   },
 
+  bindinput(e){
+    let { value } = e.detail
+    this.setData({
+      value: value
+    })
+    console.log(value)
+    if(this.data.value == '')return;
+    request({
+      url: '/goods/qsearch',
+      data:{
+        query: this.data.value
+      }
+    }).then(res=>{
+      console.log(res)
+      this.setData({
+        searchlist: res.data.message
+      })
+    })
+  }
   
 })
